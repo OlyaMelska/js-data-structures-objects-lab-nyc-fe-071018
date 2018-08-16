5392ca1a3ca5fb22eb83fe5b209af14476fa2e90
Olha Melska <olya.melska@gmail.com>
2018-08-16 15:18:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  const newObj = Object.assign({}, driver, { [key]: value });
-  return newObj;
+  const newDriver = Object.assign({}, driver, { [key]: value });
+  return newDriver;
 }
 
 
